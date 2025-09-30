7d320934f7a0f0e76686cc9d20fed69955143bb6
Csaba Nagy <csaba@geo.me>
2025-09-30 13:46:12 +0100

    First change

--- linter.py
+++ linter.py
@@ -1,10 +1,26 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from SublimeLinter.lint import RubyLinter, LintMatch
 
+"""This module exports the Flay plugin class."""
 
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
+class Flay(RubyLinter):
+    cmd = 'flay'
+    regex = r'\s*(?P<filename>.+):(?P<line>\d+)'
     multiline = False
+
     defaults = {
-        'selector': 'source.python'
+        'executable': 'flay',
+        'selector': 'source.ruby',
+        'args': ['-#']
     }
+    # , '${file_on_disk}'
+    cmd = ('${executable}', '${args}' , '${tempfile}')
+    tempfile_suffix = '-'
+    default_type = WARNING
+
+    def split_match(self, match):
+        """Add near detail to error dict."""
+
+        error = super().split_match(match)
+        error['error_type'] = 'error' if float(error['message']) > self.settings.get('threshold') else 'warning'
+
+        return error
